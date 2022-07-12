31b7b67dacb4190776f19feb89ee5dc41ed2c9dc
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-07-11 20:16:09 -0700

    created basic UI setup with routers
    - implemented with the Home, Register, Login and Navbar components to start
    - installed relevant npm packages to begin building

--- swimster-ui/src/components/App/App.jsx
+++ swimster-ui/src/components/App/App.jsx
@@ -1,12 +1,23 @@
 import * as React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { style } from './style'
+import Home from '../Home/Home'
+import Login from '../Login/Login'
+import Register from '../Register/Register'
+import Navbar from '../Navbar/Navbar'
 
 const App = () => {
   return (
-    <div className={style.container}>
-      <button className={style.button}>Login</button>
-      <button className={style.button}>Logout</button>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <main className={style.app}>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
