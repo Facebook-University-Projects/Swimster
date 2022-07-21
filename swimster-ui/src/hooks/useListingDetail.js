afc6fefae188dbdd55383379d8dcdac4d8f72df8
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-07-20 19:55:39 -0700

    added functionality for making reservation
    - v1 of making reservation UI (no validation, just pure vacuum)
    - used react-hook-form to capture date and time data

--- swimster-ui/src/hooks/useListingDetail.js
+++ swimster-ui/src/hooks/useListingDetail.js
@@ -1,15 +1,21 @@
 import { useState, useEffect } from 'react'
 import { useAuthContext, isUserAuthenticated } from "../contexts/auth"
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 import apiClient from "../services/apiClient"
 
 export const useListingDetail = listingId => {
     const { user, initialized } = useAuthContext()
+    const { register, handleSubmit } = useForm()
     const [isFetching, setIsFetching] = useState(false)
+    const [isProcessing, setIsProcessing] = useState(false)
+    const navigate = useNavigate()
     const [error, setError] = useState(null)
     const [listing, setListing] = useState({})
 
     const isAuthenticated = isUserAuthenticated(user, initialized)
 
+    // fetches the listing when on user click
     useEffect(() => {
         const fetchListing = async () => {
             setIsFetching(true)
@@ -24,9 +30,33 @@ export const useListingDetail = listingId => {
         if (isAuthenticated) fetchListing()
     }, [listingId, isAuthenticated])
 
+    // handler for making a reservation for the current listing
+    const onSubmit = async formData => {
+        setIsProcessing(true)
+
+        const formattedFormData = {
+            reservationDate: formData.reservationDate,
+            startTime: formData.reservationStartTime,
+            endTime: formData.reservationEndTime,
+            guests: 4, // value to be changed
+        }
+
+        const { data, error } = await apiClient.createReservation(JSON.stringify(formattedFormData), listingId)
+        if (error) setError(error)
+        if (data?.reservation) {
+            // will create reservations context in next PR, for now just updating database
+            navigate('/')
+        }
+        setIsProcessing(false)
+    }
+
     return {
         listing,
         error,
         isFetching,
+        isProcessing,
+        register,
+        handleSubmit,
+        onSubmit,
     }
 }
