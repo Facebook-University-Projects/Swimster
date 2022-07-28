3fc05a4afa7f13cff896b995b19730fbe1def4f0
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-07-27 23:56:22 -0700

    implemented upload and fetch S3 funcitons
    - built two functions - one uploads images to S3 using the PutObject command and another fetches an image from S3 using the GetObject command
    - created a helper function that converts a readableStream into string
    - logging results for testing

--- swimster-api/s3.js
+++ swimster-api/s3.js
@@ -5,3 +5,48 @@ const s3BucketInfo = getAwsS3Info()
 
 const s3 = new S3Client({ region: s3BucketInfo.bucketRegion })
 
+// converts readableStream to string
+const streamToStr = stream => new Promise((resolve, reject) => {
+    const chunks = []
+    stream.on("data", chunk => chunks.push(chunk))
+    stream.on("error", reject)
+    stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf-8")))
+})
+
+// uploads an image to s3
+const uploadImageToS3 = async image => {
+    try {
+        const uploadParams = {
+            Bucket: s3BucketInfo.bucketName,
+            Key: image.filename,
+            Body: image.path,
+        }
+        const result = await s3.send(new PutObjectCommand(uploadParams))
+        console.log('result: ', result);
+        return result
+    } catch (error) {
+        console.log("AWS Uploading Error:", error)
+    }
+}
+
+// gets an image from s3
+const fetchImageFromS3 = async imageKey => {
+    try {
+        const uploadParams = {
+            Bucket: s3BucketInfo.bucketName,
+            Key: imageKey,
+        }
+        const result = await s3.send(new GetObjectCommand(uploadParams))
+        console.log('result: ', result);
+        const bodyContents = await streamToStr(result.Body)
+        console.log('bodyContents: ', bodyContents);
+        return bodyContents
+    } catch (error) {
+        console.log("AWS Fetching Error:", error)
+    }
+}
+
+module.exports = {
+    uploadImageToS3,
+    fetchImageFromS3,
+}
