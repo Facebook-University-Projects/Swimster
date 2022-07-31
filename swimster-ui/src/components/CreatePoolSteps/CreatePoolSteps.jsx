676ddae36305b6d4e4ec4929f77400f7a1f72b33
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-07-30 18:04:22 -0700

    implemented image removal during upload
    - finished design of image uploading in CreatePool component
    - allowed users to select to remove an image from being uploaded, making it more user-friendly

--- swimster-ui/src/components/CreatePoolSteps/CreatePoolSteps.jsx
+++ swimster-ui/src/components/CreatePoolSteps/CreatePoolSteps.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { amenities } from '../Amenities/Amenities'
+import imageUploadIcon from '../../assets/imageUploadIcon.svg'
+import cancelButtonIcon from '../../assets/cancelButtonIcon.svg'
 
 const style = {
     formCreatePoolHeader: 'font-semibold text-3xl col-span-3 mb-3',
@@ -7,7 +9,16 @@ const style = {
     formImagesUploadTitle: 'font-semibold text-2xl',
     formImagesUploadSubtitle: 'font-semibold text-sm text-gray-500',
     imageUploadButton: 'hidden',
-    realImageUpload: 'bg-red-400 p-3 rounded-lg',
+    imageUploadContentContainer: 'grid grid-cols-3 place-items-center gap-x-2 gap-y-5 border-2 p-4 border-gray-400 rounded-md h-96',
+    imageUploadLabel: 'bg-blue-700 text-gray-200 font-semibold py-2 text-center rounded-md cursor-pointer col-span-3',
+    renderSelectedImagesContainer: 'col-span-3',
+    selectedImage: 'w-36 h-36 object-cover rounded-lg col-span-1',
+    selectedImageContainer: 'flex flex-col',
+    selectedImageCancelButton: 'bg-red-50 w-4 h-4 rounded-full cursor-pointer self-end absolute',
+    imageUploadIconImage: 'col-start-2',
+    formImagesUploadButtons: 'col-span-3 flex justify-end space-x-4',
+    formImagesUploadBackButton: 'bg-blue-700 p-3 w-1/4 rounded-md cursor-pointer font-semibold text-gray-200',
+    formImagesUploadContinueButton: 'bg-blue-700 p-3 w-1/4 rounded-md cursor-pointer font-semibold text-gray-200',
     formAmenitiesHeader: 'font-semibold text-2xl col-span-3',
     formDimensionsHeader: 'font-semibold text-2xl col-span-3',
     amenitiesContainer: 'col-span-3',
@@ -24,20 +35,28 @@ const style = {
     formStepConfirmButton: 'bg-blue-700 py-3 col-start-3 col-span-1 rounded-md mt-4 cursor-pointer font-semibold text-gray-200',
 }
 
-const RenderSelectedPhotos = ({ images }) => {
+const RenderSelectedImages = ({ selectedImages, setSelectedImages }) => {
+    const removeSelectedImage = imageIdx => {
+        const newSelectedImages = selectedImages.filter(image => image != selectedImages[imageIdx])
+        setSelectedImages([...newSelectedImages])
+    }
+
     return (
-        <div>
-            {images.length > 0 ? (
+        <div className={style.imageUploadContentContainer}>
+            {selectedImages.length > 0 ? (
                 <>
-                    {images.map(image => {
+                    {selectedImages.map((image, index) => {
                         return (
-                            <img key={image} src={image}/>
+                            <div key={index} className={style.selectedImageContainer}>
+                                <img className={style.selectedImageCancelButton} src={cancelButtonIcon} onClick={() => removeSelectedImage(index)}/>
+                                <img className={style.selectedImage} src={image}/>
+                            </div>
                         )
                     })}
                 </>
             ) : (
                 <>
-                    None
+                    <img className={style.imageUploadIconImage} src={imageUploadIcon} alt="image upload icon" />
                 </>
             )}
         </div>
@@ -67,7 +86,6 @@ const CreatePoolSteps = ({ step, nextStep, prevStep, selectedImages, setSelected
     }, [step])
 
     const handleImageSelect = e => {
-        console.log('e: ', e.target.files);
         if (e.target.files) {
             const imagesArr = Array.from(e.target.files).map(file => URL.createObjectURL(file))
 
@@ -138,14 +156,17 @@ const CreatePoolSteps = ({ step, nextStep, prevStep, selectedImages, setSelected
                         onChange={handleImageSelect}
                         // {...register("poolImages"s)}
                     />
-                    <div>
-                        <label htmlFor="file" className={style.realImageUpload}>UPLOAD</label>
+                    <label htmlFor="file" className={style.imageUploadLabel}>Upload</label>
+                    <div className={style.renderSelectedImagesContainer}>
+                        <RenderSelectedImages
+                            selectedImages={selectedImages}
+                            setSelectedImages={setSelectedImages}
+                        />
                     </div>
-                    <div>
-                        <RenderSelectedPhotos images={selectedImages}/>
+                    <div className={style.formImagesUploadButtons}>
+                        <button className={style.formImagesUploadBackButton} onClick={prevStep}>Back</button>
+                        <button className={style.formImagesUploadContinueButton} onClick={nextStep}>Continue</button>
                     </div>
-                    <button className={style.formStepBackButton} onClick={prevStep}>Back</button>
-                    <button className={style.formStepBackButton} onClick={nextStep}>Continue</button>
                 </>
             )
         case 3:
