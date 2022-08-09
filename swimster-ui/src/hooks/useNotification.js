33af26e3637c344eaa2e89113ec6c54e7e3bf3e9
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-08-09 13:46:47 -0700

    added notification custom hook

---
+++ swimster-ui/src/hooks/useNotification.js
@@ -0,0 +1,30 @@
+import { useSnackbar } from "notistack"
+
+export const useNotification = () => {
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar()
+
+    const setSuccess = (message) => {
+        enqueueSnackbar(message, {
+            variant: "success",
+            anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "center",
+            }
+        })
+    }
+
+    const setError = (message) => {
+        enqueueSnackbar(message, {
+            variant: "error",
+            anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "left"
+            }
+        })
+    }
+
+    return {
+        setSuccess,
+        setError,
+    }
+}
